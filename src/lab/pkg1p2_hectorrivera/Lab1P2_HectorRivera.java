cb7c5a32ffe68deefe11711825e1b0a5be2735e4
unknown <hrolriverav@gmail.com>
2024-01-19 14:04:50 -0600

    commit2

--- src/lab/pkg1p2_hectorrivera/Lab1P2_HectorRivera.java
+++ src/lab/pkg1p2_hectorrivera/Lab1P2_HectorRivera.java
@@ -4,17 +4,36 @@
  */
 package lab.pkg1p2_hectorrivera;
 
+import java.util.Scanner;
+
 /**
  *
  * @author hrolriverav
  */
 public class Lab1P2_HectorRivera {
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
+    static Scanner sc = new Scanner(System.in);
     
+    public static void main(String[] args) {
+        System.out.println("---Menu---");
+        System.out.println("1.Registrar Usario \n2. Listar Todo \n3.Listar Por Dominio\n4.Salir");
+        int opc = sc.nextInt();
+        while(opc!=4){
+            switch(opc){
+                case 1:
+                    
+                    break;
+                case 2:
+                    
+                    break;
+                case 3:
+                    
+                    break;
+                default:
+                    System.out.println("Ingrese una opcion valida");
+            }
+            System.out.println("---Menu---");
+            System.out.println("1.Registrar Usario \n2. Listar Todo \n3.Listar Por Dominio\n4.Salir");
+        }
+    }
 }
+    
